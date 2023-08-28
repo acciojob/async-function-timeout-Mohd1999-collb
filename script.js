ab9aa81c3ca532277254ace97bf57f4c8e14a1fc
Mohd Talib <96992202+Mohd1999-collb@users.noreply.github.com>
2023-08-29 00:55:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const text = document.getElementById("text");
+const delay = document.getElementById("delay");
+const btn = document.getElementById("btn");
+const output = document.getElementById("output");
+
+
+console.log(btn)
+
+btn.addEventListener('click', ()=>{
+    callMe(text.value, delay.value);
+})
+
+
+async function callMe(text, delay){
+    let str = await text;
+    setTimeout(()=>{
+     output.innerText = str;
+    }, delay);
+}
